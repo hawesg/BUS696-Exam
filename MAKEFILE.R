30a77803338bb98e9edb6f8a8cc0ba95e1409a7b
Garrett Hawes <ghawes@uci.edu>
2019-10-28 12:49:22 -0700

    restructured files

---
+++ MAKEFILE.R
@@ -0,0 +1,3 @@
+rmarkdown::render('./exam/exam-dark.Rmd', output_file = 'output/exam-dark.html')
+rmarkdown::render('./exam/exam-light.Rmd', output_file = 'output/exam-light.html')
+rmarkdown::render('./exam/exam-material.Rmd', output_file = 'output/exam-material.html')
